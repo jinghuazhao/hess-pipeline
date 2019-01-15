ae0d995fc98a360490984968707d75190feab95c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-01-15 16:40:09 +0000

    GIANT height example

--- height/height.sh
+++ height/height.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
-# 27-2-2017 MRC-Epid JHZ
+# 15-1-2019 MRC-Epid JHZ
 
-awk '{
-  FS=OFS="\t";
-  t=NR
-  if(NR==1) t="Pos"
-  if(NR==1) print "SNP","pos","A1","A2","Z", "N"
-  else print $1,t,$2,$3,$5/$6,$8
-}' /genetics/data/twas/14-10-16/GIANT/GIANT_HEIGHT_Wood_et_al_2014_publicrelease_HapMapCeuFreq.txt \
-   | sort -k1,1 > height
+export HEIGHT=https://portals.broadinstitute.org/collaboration/giant/images/0/01/GIANT_HEIGHT_Wood_et_al_2014_publicrelease_HapMapCeuFreq.txt.gz
 
-/genetics/bin/hess/hess.sh height
+wget -qO- $HEIGHT | \
+awk 'NR>1' | \
+sort -k1,1 | \
+join -13 -21 snp150.txt - | \
+awk '($9!="X" && $9!="Y" && $9!="Un"){if(NR==1) print "SNP CHR BP A1 A2 Z N"; else print $1,$2,$3,$4,$5,$7/$8,$10}' | \
+gzip -f > height.tsv.gz
 
-rm height
+#  SNP - rs ID of the SNP (e.g. rs62442).
+#  CHR - Chromosome number of the SNP. This should be a number between 1 and 22.
+#  BP - Base pair position of the SNP.
+#  A1 - Effect allele of the SNP. The sign of the Z-score is with respect to this allele.
+#  A2 - The other allele of the SNP.
+#  Z - The Z-score of the SNP.
+#  N - Sample size of the SNP.
+
+export HESS=/scratch/jhz22/hess
+for chrom in $(seq 22)
+do
+    python hess.py \
+        --local-hsqg height \
+        --chrom $chrom \
+        --bfile $hess/1kg_eur_1pct/1kg_eur_1pct_chr${chrom} \
+        --partition $hess/nygcresearch-ldetect-data-ac125e47bf7f/EUR/fourier_ls-chr${chrom}.bed \
+        --out step1
+done
+python hess.py --prefix step1 --out step2
