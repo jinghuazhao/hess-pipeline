cb3669c8c3f507b900f487dff460e5cf9f030d4b
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-01-15 16:40:09 +0000

    st.sh

---
+++ st.sh
@@ -0,0 +1,12 @@
+# 15-1-2019 JHZ
+
+git add README.md
+git commit -m "README"
+git add hess.*
+git commit -m "HESS"
+git add height
+git commit -m "GIANT height example"
+git add test
+git commit -m "sample test"
+git add st.sh
+git commit -m "st.sh"
