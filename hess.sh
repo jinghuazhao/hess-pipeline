6ceb77ee457ad940b0646fe933c745dfa3d58f2b
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-01-16 15:12:35 +0000

    HESS

--- hess.sh
+++ hess.sh
@@ -17,17 +17,19 @@ fi
 cd $wd
 ln -fs ${pwd}/$trait
 
-# Step 1 - setup/eigenvalues and projections
+# Step 1 - estimate eigenvalues and projections
 
-parallel -j5 --env HESS \
-             --env trait \
-             'python $HESS/hess.py \
-                     --local-hsqg ../$trait \
-                     --chrom {} \
-                     --bfile $HESS/1kg_eur_1pct/1kg_eur_1pct_chr{} \
-                     --partition $HESS/nygcresearch-ldetect-data-ac125e47bf7f/EUR/fourier_ls-chr{}.bed \
-                     --out $trait' ::: $(seq 22)
+parallel -j5 --env HESS --env trait 'python $HESS/hess.py \
+        --local-hsqg $trait \
+        --chrom {} \
+        --bfile $HESS/1kg_eur_1pct/1kg_eur_1pct_chr{} \
+        --partition $HESS/nygcresearch-ldetect-data-ac125e47bf7f/EUR/fourier_ls-chr{}.bed \
+        --out $trait' ::: $(seq 22)
 
 # Step 2 - compute local SNP heritability
   
 python $HESS/hess.py --prefix $trait --out $trait.h2g
+
+# Step x -- perform additional analyses
+
+cd -
